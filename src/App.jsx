1e468ffd7a2e1c7618eb17522a317e9d458bb1d4
saimakhanac <saimakhan0zy0@gmail.com>
2025-09-04 02:47:56 +0530

    Create vite and configured tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline text-green-800">Hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
